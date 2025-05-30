27692cbd3c0426f05844312f85bb66692af170d7
kevinnat123 <knathanael09@gmail.com>
2025-05-31 00:01:54 +0700

    [ Generate Jadwal ] upload jadwal

--- controller/admin/generateJadwalController.py
+++ controller/admin/generateJadwalController.py
@@ -27,22 +27,35 @@ def generateJadwal_index():
 @login_required
 def generate_jadwal():
     print(f"{'[ CONTROLLER ]':<15} Generate Jadwal")
-    data_dosen = dao.get_dosen()
-    data_matkul = dao.get_matkul()
-    data_ruang = dao.get_kelas()
 
-    best_schedule = ga.genetic_algorithm(
-        data_matkul, data_dosen, data_ruang, 
-        ukuran_populasi=75, jumlah_generasi=1, peluang_mutasi=0.05
-    )
+    try:
+        data_dosen = dao.get_dosen()
+        data_matkul = dao.get_matkul()
+        data_ruang = dao.get_kelas()
 
-    # grouped = defaultdict(list)
-    # for jadwal in best_schedule:
-    #     grouped[jadwal.program_studi].append(jadwal)
+        best_schedule = ga.genetic_algorithm(
+            data_matkul, data_dosen, data_ruang, 
+            ukuran_populasi=75, jumlah_generasi=1, peluang_mutasi=0.05
+        )
 
-    # ga.export_jadwal_to_excel(best_schedule, "jadwal_kuliah.xlsx", data_matkul, data_dosen)
-    
-    return jsonify({ 'data': best_schedule })
+        filename = "Jadwal_Kuliah_Semester-" + session['academic_details']['semester_depan'] + "_" + session['academic_details']['tahun_ajaran_berikutnya'].replace("/", "-")
+
+        dao.upload_jadwal(best_schedule)
+    except Exception as e:
+        print(f"{'[ CONTRO ERROR ]':<15} Error: {e}")
+        return jsonify({ 'status': False, 'message': 'Terjadi kesalahan sistem. Harap hubungi Admin.' })
+
+    return jsonify({ 'status': True, 'data': best_schedule, 'filename': filename })
+
+@generateJadwal.route("/generate_jadwal/upload_jadwal", methods=['POST'])
+@login_required
+def upload_jadwal():
+    print(f"{'[ CONTROLLER ]':<15} Upload Jadwal")
+    if session['user']['role'] == "ADMIN":
+        req = request.get_json('data')
+        data = dao.upload_jadwal(req['jadwal'])
+
+    return jsonify( data )
 
 @generateJadwal.route("/generate_jadwal/download_excel", methods=['POST'])
 @login_required
@@ -55,14 +68,14 @@ def inputpricebystore_downloadExcel():
     data_dosen = dao.get_dosen()
     data_matkul = dao.get_matkul()
 
-    excel = data_excel(ga_result, data_matkul, data_dosen)
-    return send_file(excel, as_attachment=True, attachment_filename=f"{filename}.xls")
+    excel = export_jadwal_to_excel(ga_result, data_matkul, data_dosen)
+    return send_file(excel, as_attachment=True, download_name=f"{filename}.xls")
 
 def get_current_datetime():
     # return datetime.now().strftime('%d-%m-%Y %I-%M-%S')  # 12-hour format
     return datetime.now().strftime('%d-%m-%Y %H-%M-%S')  # 24-hour format
 
-def data_excel(jadwal_list, matakuliah_list, dosen_list):
+def export_jadwal_to_excel(jadwal_list, matakuliah_list, dosen_list):
     print(f"{'[ CONTROLLER ]':<15} Building File Excel")
     output = BytesIO()
 
@@ -113,7 +126,6 @@ def data_excel(jadwal_list, matakuliah_list, dosen_list):
             for col_idx, attr in enumerate(fixed_headers):
                 if attr == "nama_matkul":
                     value = jadwal['kode_matkul']
-                    print('value attr matkul', value)
                     value = next((m['nama'] for m in matakuliah_list if m['kode'] == value[:-1] or m['kode'] == value[:-4]), None)
                 elif attr == "nama_dosen":
                     value = jadwal['kode_dosen']
@@ -149,12 +161,15 @@ def data_excel(jadwal_list, matakuliah_list, dosen_list):
         start_col = len(fixed_headers) + 2
         row_idx = 0
         worksheet.write(row_idx, start_col, "NIP", format_header)
-        worksheet.write(row_idx, start_col + 1, "Beban SKS", format_header)
+        worksheet.write(row_idx, start_col + 1, "Nama", format_header)
+        worksheet.write(row_idx, start_col + 2, "Beban SKS", format_header)
         row_idx += 1
         
         for nip, sks in dict(sorted(beban_dosen.items())).items():
+            nama_dosen = next((d['nama'] for d in dosen_list if d['nip'] == nip), None)
             worksheet.write(row_idx, start_col, nip)
-            worksheet.write(row_idx, start_col + 1, sks)
+            worksheet.write(row_idx, start_col + 1, nama_dosen)
+            worksheet.write(row_idx, start_col + 2, sks)
             row_idx += 1
 
     # Simpan workbook
--- dao/admin/generateJadwalDao.py
+++ dao/admin/generateJadwalDao.py
@@ -1,6 +1,12 @@
 from dao import Database
 from config import MONGO_DB, MONGO_LECTURERS_COLLECTION as db_dosen, MONGO_OPEN_COURSES_COLLECTION as db_matkul, MONGO_CLASSES_COLLECTION as db_kelas
+from config import MONGO_SCHEDULE_COLLECTION as db_jadwal
 from flask import session
+
+class CustomError(Exception):
+    def __init__(self, error_dict):
+        self.error_dict = error_dict
+        super().__init__(str(error_dict))
 
 class generateJadwalDao:
     def __init__(self):
@@ -20,3 +26,43 @@ class generateJadwalDao:
         print(f"{'':<7}{'[ DAO ]':<8} Get kelas")
         result = self.connection.find_many(db_kelas)
         return result['data'] if result and result.get('status') else []
+    
+    def upload_jadwal(self, jadwal):
+        print(f"{'':<7}{'[ DAO ]':<8} Upload Jadwal")
+        result = { 'status': False }
+
+        try:
+            u_id = str(session['academic_details']['semester_depan']) + str(session['academic_details']['tahun_ajaran_berikutnya'])
+            res = self.connection.find_one(db_jadwal, {'u_id': u_id})
+            data = {
+                "u_id": u_id,
+                "jadwal": jadwal
+            }
+            if (res['status'] == True):
+                if session['user']['role'] == "ADMIN":
+                    res = self.connection.update_one(db_jadwal, data)
+
+                    if res['status'] == True:
+                        result.update({ 'message': 'Jadwal untuk semester ' + session['academic_details']['semester_depan'] + ' tahun ajaran ' + session['academic_details']['tahun_ajaran_berikutnya'] + ' berhasil diperbaharui!' })
+                    else:
+                        raise CustomError({ 'message': res['message'] })
+            else:
+                if session['user']['role'] == "ADMIN":
+                    res = self.connection.insert_one(db_jadwal, data)
+
+                    if res['status'] == True:
+                        result.update({ 'message': 'Jadwal untuk semester ' + session['academic_details']['semester_depan'] + ' tahun ajaran ' + session['academic_details']['tahun_ajaran_berikutnya'] + ' berhasil ditambahkan!' })
+                    else:
+                        raise CustomError({ 'message': res['message'] })
+
+            result.update({ 'status': True })
+        except CustomError as e:
+            result.update({ "message": f"{e.error_dict.get('message')}" })
+            if e.error_dict.get('target'):
+                result.update({ 'target': e.error_dict.get('target') })
+        except Exception as e:
+            print(f"{'':<15} Error: {e}")
+            result.update({ 'message': 'Terjadi kesalahan sistem. Harap hubungi Admin.' })
+
+        print(f"{'':<15} Result: {result}")
+        return result
