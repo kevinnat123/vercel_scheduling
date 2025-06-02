359b4da0c66c7bfb43044f0211af63ac6bc00309
kevinnat123 <knathanael09@gmail.com>
2025-06-02 10:17:22 +0700

    [ Blueprint Mongo ] Hapus sensitive

--- dao/__init__.py
+++ dao/__init__.py
@@ -28,7 +28,7 @@ class Database:
     def find_one(self, collection_name, filter):
         try:
             collection = self.get_collection(collection_name)
-            result = collection.find_one(self.to_uppercase(filter))
+            result = collection.find_one(filter)
             if result:
                 result['_id'] = str(result['_id']) 
                 del result['_id']
@@ -40,7 +40,7 @@ class Database:
     def find_many(self, collection_name, filter={}, per_page=None, sort=None):
         try:
             collection = self.get_collection(collection_name)
-            query_filter = self.to_uppercase(filter)
+            query_filter = filter
             
             cursor = collection.find(query_filter)
             if sort:
@@ -61,7 +61,7 @@ class Database:
     def find_many_page(self, collection_name, filter={}, page=1, per_page=10, sort=None):
         try:
             collection = self.get_collection(collection_name)
-            cursor = collection.find(self.to_uppercase(filter)).skip((page - 1) * per_page).limit(per_page)
+            cursor = collection.find(filter).skip((page - 1) * per_page).limit(per_page)
             if sort:
                 cursor = cursor.sort(sort)
             
@@ -69,7 +69,7 @@ class Database:
             for doc in data:
                 doc.pop('_id', None)  # Hapus _id, jika ada
 
-            total_documents = collection.count_documents(self.to_uppercase(filter))
+            total_documents = collection.count_documents(filter)
             
             return {'status': True, 'data': data, 'total_documents': total_documents, 'message': 'Data ditemukan'}
         except errors.PyMongoError as e:
@@ -94,7 +94,7 @@ class Database:
     def update_one(self, collection_name, filter, update_data, sensitive = False):
         try:
             collection = self.get_collection(collection_name)
-            update_result = collection.update_one(self.to_uppercase(filter), {'$set': update_data if sensitive else self.to_uppercase(update_data)})
+            update_result = collection.update_one(filter, {'$set': update_data if sensitive else self.update_data})
             if update_result.modified_count > 0:
                 return {'status': True, 'message': 'Data berhasil diperbarui'}
             return {'status': False, 'message': 'Tidak ada data yang diperbarui'}
@@ -104,7 +104,7 @@ class Database:
     def update_many(self, collection_name, filter, update_data, sensitive = False):
         try:
             collection = self.get_collection(collection_name)
-            update_result = collection.update_many(self.to_uppercase(filter), {'$set': update_data if sensitive else self.to_uppercase(update_data)})
+            update_result = collection.update_many(filter, {'$set': update_data if sensitive else self.update_data})
             if update_result.modified_count > 0:
                 return {'status': True, 'modified_count': update_result.modified_count, 'message': 'Data berhasil diperbarui'}
             return {'status': False, 'modified_count': 0, 'message': 'Tidak ada data yang diperbarui'}
@@ -114,7 +114,7 @@ class Database:
     def delete_one(self, collection_name, filter):
         try:
             collection = self.get_collection(collection_name)
-            delete_result = collection.delete_one(self.to_uppercase(filter))
+            delete_result = collection.delete_one(filter)
             if delete_result.deleted_count > 0:
                 return {'status': True, 'message': 'Data berhasil dihapus'}
             return {'status': False, 'message': 'Data tidak ditemukan'}
@@ -124,7 +124,7 @@ class Database:
     def delete_many(self, collection_name, filter):
         try:
             collection = self.get_collection(collection_name)
-            delete_result = collection.delete_many(self.to_uppercase(filter))
+            delete_result = collection.delete_many(filter)
             if delete_result.deleted_count > 0:
                 return {'status': True, 'deleted_count': delete_result.deleted_count, 'message': 'Data berhasil dihapus'}
             return {'status': False, 'deleted_count': 0, 'message': 'Tidak ada data yang dihapus'}
@@ -134,7 +134,7 @@ class Database:
     def count_documents(self, collection_name, filter={}):
         try:
             collection = self.get_collection(collection_name)
-            return {'status': True, 'count': collection.count_documents(self.to_uppercase(filter))}
+            return {'status': True, 'count': collection.count_documents(filter)}
         except errors.PyMongoError as e:
             return {'status': False, 'count': 0, 'message': f'Error pymongo: {e}'}
     
