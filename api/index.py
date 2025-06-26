ee5ca99b08a3820dd89ee0f04ec4b4b49f59a4c0
kevinnat123 <knathanael09@gmail.com>
2025-06-26 14:23:30 +0700

    [ First Success Deployment ]

--- api/index.py
+++ api/index.py
@@ -7,14 +7,3 @@ print("[Vercel] Flask App Created")
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0",debug=True)
-
-# from pymongo import MongoClient
-
-# def handler(request, response):
-#     try:
-#         client = MongoClient(os.getenv("MONGO_URI"))
-#         db = client['db_course_scheduling']
-#         # db = client.get_database()  # atau db = client['db_course_scheduling']
-#         return response.json({"status": True, "msg": "MongoDB Connected"})
-#     except Exception as e:
-#         return response.json({"status": False, "error": "💥 FAILED " + str(e) })
