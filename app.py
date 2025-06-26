c0f9ce99019f8581418d0a737034ee727a05fa43
kevinnat123 <knathanael09@gmail.com>
2025-06-26 09:51:44 +0700

    [ APP ]

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from __init__ import create_app
+import os
 
 app = create_app()
-app.secret_key = 'your_secret_key'  # Add your secret key
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
